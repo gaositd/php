4567881a1ea26cdad1b9ad2e55a10c8968e5933b
Alvaro Gamez Chavez <gaositd@gmail.com>
2023-05-08 19:26:57 -0600

    avance a leer y cargar archivos

--- portfolio.php
+++ portfolio.php
@@ -1,15 +1,5 @@
 <?php include 'header.php'; ?>
 <?php include 'cxn.php'; ?>
-<?php
-  if($_POST){
-    print_r($_POST);
-    $nombre = $_POST['nombre'];
-    // $cxnPortfolio = new cxn();
-    // $ruta   = $_POST['ruta'];
-    // $qry = "INSERT INTO `fotos` (`id`, `nombre`, `ruta`) VALUES (NULL, '".$nombre."', '".$ruta."')";
-    // $cxnPortfolio.loadSQL($qry);
-  }
-?>
 <div class="container">
   <div class="row">
     <div class="col-md-6">
@@ -19,54 +9,39 @@
         </div>
         <div class="card-body">
           <form
-            action="portfolio.php"
-            method="post"
-            id="portfolio"
-            name="portfolio"
-            enctype="multipart/form-data"
-          >
-            <label
-              for="nombre"
-              id="labelNombre"
-              class="form-label"
-              >
+            action="portfolio.php" method="post" id="portfolio" name="portfolio" enctype="multipart/form-data" >
+            <label for="nombre" id="labelNombre" class="form-label" >
               Nombre
             </label>
-            <input
-              type="text"
-              name="nombre"
-              id="nombre"
-              class="form-control"
-              placeholder="Nombre del proyecto"
-              aria-placeholder="Nombre del proyecto"
-            />
+            <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Nombre del proyecto" aria-placeholder="Nombre del proyecto" />
             <label
-              for="web-path"
+              for="web_path"
               id="label-file"
               name="label-file"
               class="form-label mt-2"
-            >
+              >
               Ruta Web
             </label>
             <input
-              type="text"
-              name="web-path"
-              id="web-path"
-              class="form-control"
+            type="text"
+            name="web_path"
+            id="web_path"
+            class="form-control"
             />
             <label
-              type="file"
-              name="ruta"
-              id="ruta"
+              for="filename"
+              id="filename-label"
+              name="filename"
               class="form-label mt-2"
             >
-              Imagen del proyecto
+              Imagen del proyecto 
             </label>
             <input
               type="file"
-              name="input-file"
-              id="input-file"
+              name="archivo"
+              id="archivo"
               class="form-control"
+              accept="image/png,image/jpeg, png, jpeg, webp, gif"
             />
             <input type="submit" value="Enviar Proyecto" id="enviar-proyecto" class="btn btn-success mt-3">
           </form>
@@ -92,7 +67,7 @@
                   src="#"
                   alt="imagen"
                   class="img-thumbnail"
-                />
+                  />
               </td>
             </tr>
           </tbody>
@@ -104,3 +79,19 @@
   
 </div>
 <?php include 'footer.php'; ?>
+<?php
+  if($_POST){
+    // print_r($_POST);
+    // print_r($_FILES);
+    $nombre = $_POST['nombre'];
+    $web_path = $_POST['web_path'];
+    $filename = $_FILES['archivo']['name'];
+
+    print_r($nombre."\n".$web_path."\n".$filename);
+    $cxnPortfolio = new cxn();
+    $ruta   = $_POST['ruta'];
+    $qry = "INSERT INTO `fotos` (`id`, `nombre`, `ruta`, `file-path`) VALUES (NULL, '".$nombre."', '".$web_path."','".$filename."');";
+    echo $qry;
+    // $cxnPortfolio.loadSQL($qry);
+  }
+?>
