7bb76aba9ed015430c1209d113fecfc84a8225c2
Alvaro Gamez Chavez <gaositd@gmail.com>
2023-05-13 14:00:43 -0600

    me atoré en el borrado de datos

--- cxn.php
+++ cxn.php
@@ -9,21 +9,33 @@
     public function __construct() {
       try {
         $this->cxn = new PDO("mysql:host=$this->server;dbname=$this->dbName", $this->user, $this->pass);
-        $this.setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // $this.setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->cxn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       }
       catch (PDOException $e) {
         echo "Error ".$e;
       } 
     }
 
-    public function loadSQL($qry){
+    public function actionsSQL($qry){//insert//delete//update
       $this->cxn->exec($qry);
       return $this->cxn->lastInsertId();
     }
 
     public function selectSQL($qry){
-      $this->cxn->exec($qry);
-      return $this->cxn->selectAll();
+      $select = $this->cxn->prepare($qry);
+      $select->execute();
+      return $select->fetchAll();
+    }
+
+    public function deleteSQL($qry){
+      $delete = $this->cxn->prepare($qry);
+      $delete->execute();
+      $this->selectSQL("SELECT * FROM `fotos` WHERE marca_borrado != 'X';");
+    }
+
+    public function closeCxn(){
+      unset(self::$cxn);
     }
 
   }
--- portfolio.php
+++ portfolio.php
@@ -2,7 +2,7 @@
 <?php include 'cxn.php'; ?>
 <div class="container">
   <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-3">
       <div class="card">
         <div class="card-header">
           Datos del protecto
@@ -48,7 +48,7 @@
         </div>
       </div>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-9">
       <div class="table-responsive">
         <table class="table table-striped">
           <thead>
@@ -56,20 +56,62 @@
               <th scope="col">Nombre</th>
               <th scope="col">WEB</th>
               <th scope="col">Foto</th>
+              <th scope="col">Acciones</th>
             </tr>
           </thead>
           <tbody>
-            <tr class="">
-              <td scope="row">ABC</td>
-              <td>nombre</td>
-              <td>
-                <img
-                  src="#"
-                  alt="imagen"
-                  class="img-thumbnail"
-                  />
-              </td>
-            </tr>
+            <?php
+              $selectCxn = new cxn();
+
+              $qry = "SELECT * FROM `fotos` WHERE marca_borrado != 'X';";
+              $fotos = $selectCxn->selectSQL($qry);
+              foreach($fotos as $foto){
+                $ruta_web = substr($foto['ruta'],8,17);
+                print_r("
+                  <tr class=''>
+                    <td class='align-self-center ms-1'>$foto[nombre]</td>
+                    <td class='ms-1'>
+                      <a 
+                        href='$foto[ruta]'
+                        title='$foto[nombre]'
+                        target='_blank'
+                        class='ms-md-auto'
+                      >
+                        $ruta_web
+                      </a>
+                    </td>
+                    <td class='ms-1'>
+                      <img
+                        src='$foto[ruta]'
+                        alt='$foto[nombre]'
+                        class='img-fluid img-thumbnail'
+                        title='$foto[nombre]'
+                        width='30px'
+                        height='auto'
+                      />
+                    </td>
+                    <td class='ms-1'>
+                      <a href='?mod=$foto[id]' class='ms-3'>
+                        <i
+                          class='fa-solid fa-eraser'
+                          title='Modificar'
+                        ></i>
+                      </a>
+                      <a 
+                        href='?del=$foto[id]'
+                        class='ms-3'
+                      > 
+                        <i 
+                          class='fa-solid fa-trash fa-xs ms-3'
+                          title='Borrar'
+                        ></i>
+                      </a>
+                    </td>
+                  </tr>
+                ");
+              }
+              $selectCxn->closeCxn();
+            ?>
           </tbody>
         </table>
       </div>
@@ -80,6 +122,7 @@
 </div>
 <?php include 'footer.php'; ?>
 <?php
+
   function fError($flag){
     echo "==>>".$flag;
     if($flag == true){
@@ -91,6 +134,7 @@
     }
   }
 
+
   if($_POST){
     $nombre = $_POST['nombre'];
     $web_path = $_POST['web_path'];
@@ -99,12 +143,35 @@
     if(strlen($nombre) <= 5){
       fError(false);
     }else{
-      $qry = "INSERT INTO `fotos` (`id`, `nombre`, `ruta`, `file-path`) VALUES (NULL, '$nombre', '$web_path','$filename');";
-      echo $qry;
+      $qry = "INSERT INTO `fotos` (`id`, `nombre`, `ruta`, `file-path`) VALUES (NULL, '".$nombre."','".$web_path."','".$filename."');";
+      $cxnPortfolio = new cxn();
+      $cxnPortfolio->actionsSQL($qry);
+      $cxnPortfolio->closeCxn();
     }
+  
+  }
 
-    // $cxnPortfolio = new cxn();
-    // $qry = "INSERT INTO `fotos` (`id`, `nombre`, `ruta`, `file-path`) VALUES (NULL, '$nombre', '$web_path','$filename');";
-    // echo $qry;
+  if($_GET){
+    // $deleteId = $_GET['del'];
+    // echo '<div class="alert alert-danger" role="alert">$deleteId</div>';
+    // $updateId = $_GET['mod'];
+    // $cxnActions = new cxn();
+    // echo '<script>alert($deleteId)</script>';
+    // if($deleteId > 0){
+      $qry="Delete From `fotos` WHERE `fotos`.`id` = ".$_GET['del'].";";
+      // $deleteId";
+      // echo '<div class="alert alert-danger" role="alert">$qry</div>';
+      $cxnActions->actionsSQL($qry);
+      $cxnActions->closeCxn();
+    // }else{//6:50:43
+    //   echo '<div class="alert alert-danger" role="alert">No entró</div>';
+    // }
+
+    // if($updateId){
+    //   // update($updateId);
+    // }
+    
+  }else{
+    echo '<script>alert("no entró")</script>';
   }
 ?>
