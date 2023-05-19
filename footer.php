292f027b350f218e2ca2c19e41e600d494dc0dce
Alvaro Gamez Chavez <gaositd@gmail.com>
2023-05-19 14:27:50 -0600

    armado de la tarjeta y menu

--- footer.php
+++ footer.php
@@ -1,4 +1,25 @@
 <?php /*echo"chmod ugo+rw images/";*/ ?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+            <a href="index.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+            <a href="portfolio.php">Portafolio</a>
+            </li>
+            <li class="nav-item">
+              <a href="closeSession.php">Cerrar</a>ass="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
  </div> <!--the main container ends -->
 </body>
 </html>
--- header.php
+++ header.php
@@ -34,8 +34,26 @@
 </head>
 <body>
   <div class="container">
-    <a href="index.php">Inicio</a>
-    <a href="portfolio.php">Portafolio</a>
-    <a href="closeSession.php">Cerrar</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+            <a href="index.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+            <a href="portfolio.php">Portafolio</a>
+            </li>
+            <li class="nav-item">
+              <a href="closeSession.php">Cerrar</a>ass="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   
   <!-- Aquí inicia el proyecto en PHP -->
